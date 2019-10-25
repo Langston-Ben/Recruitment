caa373126c88be2acb0df02fbf349dbb75056a99
Ben <benlangston2009@gmail.com>
2019-10-25 09:47:35 -0700

    14th commit - Added javascript to anchor the result page after the practice query is submitted for a better UX.

--- results.php
+++ results.php
@@ -9,11 +9,20 @@ and open the template in the editor.
         
         <?php $ptitle = "Results";
         include $_SERVER['DOCUMENT_ROOT'] . '/recruitment/common/head.php'; ?>
+       
+        <script type="text/javascript" language="javascript">
+        
+        function moveWindow() 
+        {
+            window.location.hash="jump";
+        }
+        
+        </script>
     </head>
   
-    <body>
+    <!--<body>-->
         
-     
+    <body onload="moveWindow()">
 
    <!--     <nav>
             <?php $page='home'; include $_SERVER['DOCUMENT_ROOT'].'/Recruitment/common/nav.php'; ?> 
@@ -22,6 +31,9 @@ and open the template in the editor.
     
         <h1>Results:</h1> 
         <div class="overflow"><?php if (isset($query)) {echo $query;} ?></div>
+        
+        <div class="jump"><a name="jump"></a></div>
+        
          <?php if (isset($message)) {echo $message; 
          echo "<div class='finish'<p><a href='/Recruitment/index.php' title='Home'>Home</a></p></div>";} ?>
         <?php if (isset($querylist)) {echo $querylist;} ?><br>
